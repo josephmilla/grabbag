03ddeee3ac667a7cf16039ec93a96783caa65c08
Joseph Milla <jjtmilla@gmail.com>
2015-06-06 11:22:32 -0500

    Connect Alchemy endpoint

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -24,4 +24,41 @@ angular.module('grabbagApp')
     $scope.$on('$destroy', function () {
       socket.unsyncUpdates('thing');
     });
+
+
+    $http({
+      method: 'GET',
+      url: 'http://access.alchemyapi.com/calls/text/TextGetTextSentiment?apikey=6c3f7de7adae065674ccce9289e1a2930fc4c74f',
+      params: {
+        text: 'Fuck you motherfucker',
+        outputMode: 'json',
+        showSourceText: 1
+        }
+      }).
+      success(function (data, status, headers, config) {
+        console.log("GET request was successful!");
+        console.log(data);
+
+        // var events = data.events;
+        // var lats = [];
+        // var lons = [];
+        // var coordinates = [];
+        // for (var i=0; i < events.length; i++) {
+        //   if (events && events[i] && events[i].venue) {
+        //     var venue = events[i].venue;
+        //     var address = venue.address;
+        //     var lat = address.latitude;
+        //     var lon = address.longitude;
+        //     lats.push(lat);
+        //     lons.push(lon);
+        //     coordinates.push(new google.maps.LatLng(lat, lon));
+        //   }
+        // }
+        //
+        // console.log(coordinates);
+        // $scope.coordinates = coordinates;
+      }).
+      error(function (data, status, headers, config) {
+        console.log("GET request wasn't successful");
+      });
   });
