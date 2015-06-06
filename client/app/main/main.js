48700ad19d1212af30f1f8aa7fe8ce9b8ce512ca
Joseph Milla <jjtmilla@gmail.com>
2015-06-05 21:55:15 -0500

    Initialize repository

---
+++ client/app/main/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('grabbagApp')
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
