f34f2e8582c7b21291b1f6deedb5541abe4cda1f
Joseph Milla <jjtmilla@gmail.com>
2015-06-06 19:04:46 -0500

    Transfer files from server

--- assets/js/s2t.js
+++ assets/js/s2t.js
@@ -1,119 +1,91 @@
-var final_transcript = '';
-var recognizing = false;
-var ignore_onend;
-var start_timestamp;
+var speech = {
+	kit : null,
+	on : false,
+	remaining : 0,
+	prepare : function() {
+		if (!('webkitSpeechRecognition' in window)) {
+			console.log("fail");
+		}
+		else if (speech.kit === null) {
+			speech.init();
+		}
+	},
 
-if (!('webkitSpeechRecognition' in window)) {
-  // update your chrome yo!
-  upgrade();
-} else {
-  
-  var startButton = document.createElement('input');
-  startButton.type = "button";
-  document.body.appendChild(startButton);
-  
-  startButton.style.display = 'inline-block'; // remove this and test
-  var recognition = new webkitSpeechRecognition();
-  recognition.continuous = true;
-  recognition.interimResults = true;
+	init : function() {
+		speech.kit = new webkitSpeechRecognition();
+		speech.kit.lang = speech.lang;
+		speech.kit.continuous = false;
+		speech.kit.interimResults = false;
 
-  
-  recognition.onstart = function() {
-    recognizing = true;
-    showInfo('info_speak_now');
-    start_img.src = 'mic-animate.gif';
-  };
+		speech.kit.onstart = function() {
+			speech.on = true;
+		};
+		speech.kit.onresult = function(event) {
+			speech.remaining = 0;
+			var list = event.results;
+			var transcript = "";
+			for (var i=0; i<list.length; i++) {
+				if (event.results[i].isFinal) {
+					transcript += event.results[i][0].transcript;
+				}
+				else {
+					transcript += event.results[i][0].transcript;
+				}
+			}
+			if (transcript.trim()) {
+				// a.innerHTML = transcript;
+				console.log(transcript);
+				// var param = parseUrlSearch();
+				// sendMessage({
+					// type    : "forward",
+					// message : transcript,
+					// forward : "speech",
+					// line    : param.line,
+					// lang    : param.lang,
+					// instance: param.instance,
+				// });
+			}
+			else {
+				speech.start();
+			}
+		};
+		speech.kit.onerror = function(event) {
+		};
+		speech.kit.onend = function() {
+			// speech.on = false;
+			speech.kit.start();
+			// speech.kit.stop();
+		};
+	},
 
-  recognition.onerror = function(event) {
-    if (event.error == 'no-speech') {
-      start_img.src = 'mic.gif';
-      showInfo('info_no_speech');
-      ignore_onend = true;
-    }
-    if (event.error == 'audio-capture') {
-      start_img.src = 'mic.gif';
-      showInfo('info_no_microphone');
-      ignore_onend = true;
-    }
-    if (event.error == 'not-allowed') {
-      if (event.timeStamp - start_timestamp < 100) {
-        showInfo('info_blocked');
-      } else {
-        showInfo('info_denied');
-      }
-      ignore_onend = true;
-    }
-  };
-
-  recognition.onend = function() {
-    recognizing = false;
-    if (ignore_onend) {
-      return;
-    }
-    start_img.src = 'mic.gif';
-    if (!final_transcript) {
-      showInfo('info_start');
-      return;
-    }
-    showInfo('');
-    if (window.getSelection) {
-      window.getSelection().removeAllRanges();
-      var range = document.createRange();
-      range.selectNode(document.getElementById('final_span'));
-      window.getSelection().addRange(range);
-    }
-    
-  };
-
-  // what is the result
-  recognition.onresult = function(event) {
-    var interim_transcript = '';
-    for (var i = event.resultIndex; i < event.results.length; ++i) {
-      if (event.results[i].isFinal) {
-        final_transcript += event.results[i][0].transcript;
-      } else {
-        interim_transcript += event.results[i][0].transcript;
-      }
-    }
-    final_transcript = capitalize(final_transcript);
-    final_span.innerHTML = linebreak(final_transcript);
-    interim_span.innerHTML = linebreak(interim_transcript);
-    if (final_transcript || interim_transcript) {
-      showButtons('inline-block');
-    }
-  };
+	start : function() {
+		if (!speech.on && speech.remaining < 10) {
+			speech.remaining++;
+			speech.lang = "en-US";
+			speech.prepare();
+			speech.kit.lang = speech.lang;
+			if (speech.on) {
+				speech.kit.stop();
+			}
+			speech.kit.start();
+		}
+	},
 }
 
-// chrome upgrade
-function upgrade() {
-  startButton.style.visibility = 'hidden';
-  showInfo('info_upgrade');
-}
-var two_line = /\n\n/g;
-var one_line = /\n/g;
-function linebreak(s) {
-  return s.replace(two_line, '<p></p>').replace(one_line, '<br>');
-}
-var first_char = /\S/;
-function capitalize(s) {
-  return s.replace(first_char, function(m) { return m.toUpperCase(); });
+function trigger() {
+	if (speech.kit && speech.on) {
+		speech.on = false;
+		speech.kit.stop();
+	}
+	else {
+		speech.start();
+	}
 }
 
-// remove this after it works on server
-function startButton(event) {
-  if (recognizing) {
-    recognition.stop();
-    return;
-  }
-  
-  final_transcript = '';
-  recognition.lang = select_dialect.value;
-  recognition.start();
-  ignore_onend = false;
-  final_span.innerHTML = '';
-  interim_span.innerHTML = '';
-  start_img.src = 'mic-slash.gif';
-  showInfo('info_allow');
-  showButtons('none');
-  start_timestamp = event.timeStamp;
-}
+var startButton = document.createElement('input');
+startButton.type = "button";
+document.body.appendChild(startButton);
+startButton.onclick = trigger;
+
+startButton.style.display = 'inline-block'; // remove this and test
+
